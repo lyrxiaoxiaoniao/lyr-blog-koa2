a1ca3106fef3419bd7e84e574a991a278595e2fe
liuyouren <lyr632537786@163.com>
2019-05-16 18:09:35 +0800

    update

--- controllers/upload.js
+++ controllers/upload.js
@@ -2,13 +2,15 @@ const util = require('util');
 module.exports = async (ctx, next) => {
   let data = [];
   let files = ctx.request.files.file;
+  const ispro = process.env.NODE_ENV === 'production'
+  const host = ispro ? 'www.liuyouren.top' : ctx.host
   // 区分是否是多文件上传
   if (files.length) {
     data = files.map(v => {
-      return `${ctx.host}${v.outPath}`
+      return `${host}${v.outPath}`
     });
   } else {
-    data.push(`http://${ctx.host}${files.outPath}`)
+    data.push(`http://${host}${files.outPath}`)
   }
   ctx.state.data = {
     files: data
