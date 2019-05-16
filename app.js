574914e143384ffc7d6f451bcee43a9badaab8ad
liuyouren <lyr632537786@163.com>
2019-05-16 17:44:43 +0800

    update

--- app.js
+++ app.js
@@ -4,28 +4,11 @@ const debug = require('debug')('lyr-blog-koa2');
 const koajwt = require('koa-jwt');
 const config = require('./config');
 const middlewares = require('./middlewares/index.js');
-
+const onerror = require('koa-onerror')
+onerror(app)
 middlewares(app);
 // 验证token, unless--排除某个路径不做token鉴权
 // token 在Headers中的名为Authorization的键值对中
-// app.use(function(ctx, next) {
-//     // 自定义token失效报错 移到响应中间件了
-//   return next().catch(e => {
-//     if (401 == e.status) {
-//       ctx.status = 200;
-//       ctx.body = {
-//         code: -1,
-//         success: false,
-//         error: e && e.message ? e.message : e.toString(),
-//         message: '登录失效！'
-//     }
-//     } else {
-//       throw e;
-//     }
-//   });
-// });
-
-
 app.use(
   koajwt({
     secret: config.tokenSecret
@@ -37,4 +20,9 @@ app.use(
 const router = require('./routers');
 app.use(router.routes());
 
-app.listen(config.port, () => debug(`listening on port ${config.port}`));
+// app.listen(config.port, () => debug(`listening on port ${config.port}`));
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+module.exports = app
